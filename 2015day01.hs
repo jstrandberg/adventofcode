a70178dce2ff8c6f55cd4d6fb204b87b7d067f68
Jesper Strandberg <jesper.strandberg@protonmail.com>
2021-11-26 14:45:04 +0100

    added 2015day01

---
+++ 2015day01.hs
@@ -0,0 +1,24 @@
+examples :: [(String,Int)]
+examples =
+    [("(())",0)
+    ,("()()",0)
+    ,("(((",3)
+    ,("(()(()(",3)
+    ,("))(((((",3)
+    ,("())",-1)
+    ,("))(",-1)
+    ,(")))",-3)
+    ,(")())())",-3)
+    ]
+
+what_floor :: String -> Int
+what_floor s =
+    let
+        count_up s = length $ filter (=='(') s
+        count_down s = length $ filter (==')') s
+    in
+        (count_up s) - (count_down s)
+
+main :: IO ()
+main = do
+    putStrLn "What to do..."
