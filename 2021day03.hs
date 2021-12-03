28b0149f14fa656c16891a977f110fdb90d6a8bd
Jesper Strandberg <jesper.strandberg@protonmail.com>
2021-12-03 17:03:14 +0100

    created 2021 day 3

---
+++ 2021day03.hs
@@ -0,0 +1,30 @@
+import Data.List
+
+example :: [String]
+example = ["00100"
+          ,"11110"
+          ,"10110"
+          ,"10111"
+          ,"10101"
+          ,"01111"
+          ,"00111"
+          ,"11100"
+          ,"10000"
+          ,"11001"
+          ,"00010"
+          ,"01010"
+          ]
+
+middle :: String -> Int
+middle s = div (length s) 2
+
+-- already forgot how this works...
+gamma :: [String] -> String
+gamma x = map (flip (!!) (middle $ head $ transpose x) . sort) $ transpose x
+
+epsilon :: [String] -> String
+epsilon x = map (\x -> if x == '0' then '1' else '0') $ gamma x
+
+-- stolen from iceman@stackoverflow
+binaryToInt :: String -> Int
+binaryToInt s = foldl (\a n -> a * 2 + (read [n])) 0 s
