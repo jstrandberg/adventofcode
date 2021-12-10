e54b0e59d7bb750a015d98fef9b5581a83abb8b4
Jesper Strandberg <jesper.strandberg@protonmail.com>
2021-12-10 16:05:16 +0100

    added part 2

--- 2021day07.hs
+++ 2021day07.hs
@@ -12,11 +12,25 @@ to x = (last . sort) x
 possibilities :: [Int] -> [Int]
 possibilities x = [from x .. to x]
 
-play xs = map (\n -> sum $ distance xs n) distances
+play :: [Int] -> [Int]
+play xs = map (\n -> sum $ distance xs n) (possibilities xs)
     where distance x a = map (\n -> abs $ a - n) x
+
+-- part 2 is insanely slow
+
+cost :: Int -> Int
+cost 0 = 0
+cost n = n + cost (n - 1)
+
+play' :: [Int] -> [Int]
+play' xs = map (\n -> sum $ distance xs n) (possibilities xs)
+    where distance x a = map (\n -> cost (abs $ a - n)) x
 
 result :: [Int] -> Int
 result x = head $ sort $ play x
+
+result' :: [Int] -> Int
+result' x = head $ sort $ play' x
 
 parse :: String -> [Int]
 parse s = map read $ words [if c == ',' then ' ' else c | c <- s]
@@ -25,6 +39,9 @@ main :: IO ()
 main = do
     putStrLn "What to do..."
     putStrLn $ show $ result example
-
+    putStrLn $ show $ result' example
+    {-
     file <- readFile "input.txt"
     putStrLn $ show $ result $ parse file
+    putStrLn $ show $ result' $ parse file
+    -}
