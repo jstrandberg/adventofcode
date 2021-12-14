209c8f4aa2c454e581789f6be99c1c36e13d805c
jstrandberg <jesper.strandberg@protonmail.com>
2021-12-14 22:38:59 +0100

    refactored for part 2

--- 2021day14.hs
+++ 2021day14.hs
@@ -23,30 +23,44 @@ example =
     ,"CN -> C"
     ]
 
-template :: [String] -> String
-template x = head x
+type Pair = (Char,Char)
+type Rule = (Pair,[Pair])
 
-rules :: [String] -> [(String,Char)]
-rules (_:_:xs) = map rule xs
-    where rule (a:b:_:_:_:_:c:[]) = ([a,b],c)
+parseInput :: [String] -> [(Pair,Int)]
+parseInput = pairs . head
+    where pairs []       = []
+          pairs [a]      = [((a,' '),1)]
+          pairs (a:b:xs) = ((a,b),1) : pairs (b:xs)
 
-apply :: [(String,Char)] -> String -> String
-apply _ []       = []
-apply _ [a]      = [a]
-apply r (a:b:xs) = [a] ++ (maybeToList $ lookup [a,b] r) ++ apply r (b:xs)
+compress :: Ord a => [(a,Int)] -> [(a,Int)]
+compress = foldr compress' [] . sort
+    where compress' a []     = [a]
+          compress' a (x:xs) = if fst x == fst a
+              then ((fst x, snd x + snd a) : xs)
+              else (a : x : xs)
 
-result :: [String] -> Int
-result x = mc - lc
-    where iterations = iterate (apply $ rules x) (template x)
-          occurences = map length . group . sort $ iterations !! 10
+parseRules :: [String] -> [Rule]
+parseRules = map rule . drop 2
+    where rule (a:b:_:_:_:_:c:[]) = ((a,b),[(a,c),(c,b)])
+
+apply :: [Rule] -> [(Pair,Int)] -> [(Pair,Int)]
+apply rules = foldl apply' []
+    where apply' res (pair,n) = (++) res . map (,n) . fromMaybe [pair] . lookup pair $ rules
+
+result :: Int -> [String] -> Int
+result i x = mc - lc
+    where iterations = iterate (compress . apply (parseRules x)) (parseInput x)
+          occurences = map snd . compress . map (\(pair,n) -> (fst pair,n)) $ iterations !! i
           mc = last . sort $ occurences
           lc = head . sort $ occurences
 
 main :: IO ()
 main = do
     putStrLn "What to do..."
-    putStrLn . show . result $ example
+    putStrLn . show . result 10 $ example
+    putStrLn . show . result 40 $ example
     {-
     file <- readFile "input.txt"
-    putStrLn . show . result . lines $ file
+    putStrLn . show . result 10 . lines $ file
+    putStrLn . show . result 40 . lines $ file
     -}
