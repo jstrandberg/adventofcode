5215125c666c90f19cd7dfa94ab49c74093303fe
Jesper Strandberg <jesper.strandberg@protonmail.com>
2022-10-11 14:09:01 +0200

    completed second star

--- 2015day02.hs
+++ 2015day02.hs
@@ -13,11 +13,20 @@ calc s = let
   x  = foldl min maxBound ss
   in (+) x $ sum $ map ((*) 2) ss
 
+calc' :: String -> Int
+calc' s = let
+  [l,w,h] = parse s
+  ss = [2*(l+w),2*(w+h),2*(h+l)]
+  x  = foldl min maxBound ss
+  in x + (l*w*h)
+
 main :: IO ()
 main = do
     putStrLn "What to do..."
     putStrLn $ show $ sum $ map calc example
+    putStrLn $ show $ sum $ map calc' example
     {-
     file <- readFile "input.txt"
     putStrLn $ show $ sum $ map calc $ lines file
+    putStrLn $ show $ sum $ map calc' $ lines file
     -}
