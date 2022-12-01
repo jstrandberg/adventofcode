8cac631352b4819382ca47218fc6a65b769996c5
Jesper Strandberg <jesper.strandberg@protonmail.com>
2022-12-01 13:22:13 +0100

    refactored descending sort

--- 2022day01.hs
+++ 2022day01.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Ord
 
 example :: String
 example = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
@@ -14,8 +15,10 @@ parse = foldr fn [[]] . lines
 calc :: [[Int]] -> Int
 calc = maximum . map sum
 
+-- replaced "reverse . sort" with a more efficient alternative
+-- https://ro-che.info/articles/2016-04-02-descending-sort-haskell
 calc' :: [[Int]] -> Int
-calc' = sum . take 3 . reverse . sort . map sum
+calc' = sum . take 3 . sortBy (comparing Down) . map sum
 
 main :: IO ()
 main = do
