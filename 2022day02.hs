dc0de8f8c8dd88088b5829086fabbe7554a4c5c1
Jesper Strandberg <jesper.strandberg@protonmail.com>
2022-12-02 09:48:55 +0100

    added part two

--- 2022day02.hs
+++ 2022day02.hs
@@ -12,14 +12,24 @@ score "C X" = 6 + 1
 score "C Y" = 0 + 2
 score "C Z" = 3 + 3
 
-calc :: [String] -> Int
-calc = sum . map score
+score' :: String -> Int
+score' "A X" = 0 + 3
+score' "A Y" = 3 + 1
+score' "A Z" = 6 + 2
+score' "B X" = 0 + 1
+score' "B Y" = 3 + 2
+score' "B Z" = 6 + 3
+score' "C X" = 0 + 2
+score' "C Y" = 3 + 3
+score' "C Z" = 6 + 1
 
 main :: IO ()
 main = do
   putStrLn "What to do..."
-  putStrLn $ show $ calc $ lines example
+  putStrLn $ show $ sum $ map score $ lines example
+  putStrLn $ show $ sum $ map score' $ lines example
   {-
   file <- readFile "input.txt"
-  putStrLn $ show $ calc $ lines file
+  putStrLn $ show $ sum $ map score $ lines file
+  putStrLn $ show $ sum $ map score' $ lines file
   -}
